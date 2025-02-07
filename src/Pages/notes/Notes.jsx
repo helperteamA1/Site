e6d9ad5b29ab9913dcb82bcfe271ac15b875d78a
helperteamA1 <juetunofficial@gmail.com>
2025-02-07 21:04:39 +0530

    update

---
+++ src/Pages/notes/Notes.jsx
@@ -0,0 +1,17 @@
+
+
+import { FaLock } from "react-icons/fa6";
+const Notes = () => {
+
+
+  return (
+
+           
+<div className="lock">
+<svg className="lock-icon"><FaLock size={100} /></svg>
+</div>
+    
+  )
+}
+
+export default Notes
